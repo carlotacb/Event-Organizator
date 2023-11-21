5abf7a7d7af31c4a2b651b8cac218dea9e5dad05
Carlota Catot Bragós <carkbra@gmail.com>
2023-11-21 17:29:04 +0100

    Factories

---
+++ organizator_api/app/events/infrastructure/repository_factories.py
@@ -0,0 +1,13 @@
+from app.events.domain.repositories import EventRepository
+from app.events.infrastructure.persistance.orm_event_repository import ORMEventRepository
+
+
+class EventRepositoryFactory:
+    _events_repository = None
+
+    @staticmethod
+    def create() -> EventRepository:
+        if EventRepositoryFactory._events_repository is None:
+            EventRepositoryFactory._events_repository = ORMEventRepository()
+
+        return EventRepositoryFactory._events_repository
