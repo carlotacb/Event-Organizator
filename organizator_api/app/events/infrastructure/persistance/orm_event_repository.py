3aeebc81d093dabe0a2e87a1216a87a09340433b
Carlota Catot Bragós <carkbra@gmail.com>
2023-11-21 20:15:59 +0100

    adding pragma no cover to uncompleted functions

--- organizator_api/app/events/infrastructure/persistance/orm_event_repository.py
+++ organizator_api/app/events/infrastructure/persistance/orm_event_repository.py
@@ -8,19 +8,19 @@ from app.events.infrastructure.persistance.models.orm_event import ORMEvent
 
 class ORMEventRepository(EventRepository):
     def create(self, event: Event) -> None:
-        pass
+        pass # pragma: no cover
 
     def update(self, event: Event) -> None:
-        pass
+        pass # pragma: no cover
 
     def delete(self, event_id: uuid.UUID) -> None:
-        pass
+        pass # pragma: no cover
 
     def get(self, event_id: uuid.UUID) -> Event:  # type: ignore
-        pass
+        pass # pragma: no cover
 
     def get_all(self) -> List[Event]:  # type: ignore
-        pass
+        pass # pragma: no cover
 
     def to_model(self, event: Event) -> ORMEvent:  # type: ignore
-        pass
+        pass # pragma: no cover
