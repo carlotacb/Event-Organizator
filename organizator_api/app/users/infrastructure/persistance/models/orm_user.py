0be155557216789276b7b5998269e5499edf06c2
Carlota Catot Bragós <carkbra@gmail.com>
2023-11-27 12:39:58 +0100

    Fix tests and migrations created

--- organizator_api/app/users/infrastructure/persistance/models/orm_user.py
+++ organizator_api/app/users/infrastructure/persistance/models/orm_user.py
@@ -6,7 +6,7 @@ class ORMUser(models.Model):
         db_table = "user"
 
     id = models.UUIDField(primary_key=True)
-    email = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
--- organizator_api/tests/users/infrastructure/http/test_views.py
+++ organizator_api/tests/users/infrastructure/http/test_views.py
@@ -8,18 +8,26 @@ class TestUserViews(ApiTests):
         super().setUp()
         self.event_repository.clear()
 
-    def test__given_unexpected_body__when_create_user__then_bad_request_is_returned(self) -> None:
+    def test__given_unexpected_body__when_create_user__then_bad_request_is_returned(
+        self,
+    ) -> None:
         # Given
-        body = {}
+        body = {}  # type: ignore
 
         # When
-        response = self.client.post("/organizator-api/users/new", json.dumps(body), content_type="application/json")
+        response = self.client.post(
+            "/organizator-api/users/new",
+            json.dumps(body),
+            content_type="application/json",
+        )
 
         # Then
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.content, b"Unexpected body")
 
-    def test__given_user_already_exists__when_create_user__then_conflict_is_returned(self) -> None:
+    def test__given_user_already_exists__when_create_user__then_conflict_is_returned(
+        self,
+    ) -> None:
         # Given
         body = {
             "email": "carlota@hackpc.com",
@@ -41,17 +49,27 @@ class TestUserViews(ApiTests):
         }
 
         # When
-        self.client.post("/organizator-api/users/new", json.dumps(body), content_type="application/json")
-        response = self.client.post("/organizator-api/users/new", json.dumps(body2), content_type="application/json")
+        self.client.post(
+            "/organizator-api/users/new",
+            json.dumps(body),
+            content_type="application/json",
+        )
+        response = self.client.post(
+            "/organizator-api/users/new",
+            json.dumps(body2),
+            content_type="application/json",
+        )
 
         # Then
         self.assertEqual(response.status_code, 409)
         self.assertEqual(response.content, b"User already exists")
 
-    def test__given_a_json_body_with_a_user__when_creat_user__then_the_user_is_created_and_stored_in_db(self) -> None:
+    def test__given_a_json_body_with_a_user__when_creat_user__then_the_user_is_created_and_stored_in_db(
+        self,
+    ) -> None:
         # Given
         body = {
-            "email": "carlota@hackpc.com",
+            "email": "carlota@hackupc.com",
             "password": "12345678",
             "first_name": "Carlota",
             "last_name": "Catot",
@@ -61,14 +79,18 @@ class TestUserViews(ApiTests):
         }
 
         # When
-        response = self.client.post("/organizator-api/users/new", json.dumps(body), content_type="application/json")
+        response = self.client.post(
+            "/organizator-api/users/new",
+            json.dumps(body),
+            content_type="application/json",
+        )
 
         # Then
         self.assertEqual(response.status_code, 201)
         self.assertEqual(response.content, b"User created correctly")
 
-        self.assertEqual(len(self.event_repository.get_all()), 1)
-        user = self.event_repository.get_all().pop()
+        self.assertEqual(len(self.user_repository.get_all()), 1)
+        user = self.user_repository.get_all().pop()
         self.assertEqual(user.email, "carlota@hackupc.com")
         self.assertEqual(user.password, "12345678")
         self.assertEqual(user.first_name, "Carlota")
