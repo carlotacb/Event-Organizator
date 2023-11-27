136dec448fc8af01c2da42f1d0cd5748a0206adc
Carlota Catot Bragós <carkbra@gmail.com>
2023-11-27 13:23:04 +0100

    [ORG-28] Get all users endpoint created (#10)
    * Create orm repository implementation and also tested
    
    * black fix
    
    * get all users use case with tests
    
    * create view and response all tested

--- organizator_api/app/users/infrastructure/http/views.py
+++ organizator_api/app/users/infrastructure/http/views.py
@@ -6,6 +6,8 @@ from django.views.decorators.http import require_http_methods
 from app.users.application.requests import CreateUserRequest
 from app.users.domain.use_cases.create_user_use_case import CreateUserUseCase
 from app.users.domain.exceptions import UserAlreadyExists
+from app.users.domain.use_cases.get_all_users_use_case import GetAllUsersUseCase
+from app.users.application.response import UserResponse
 
 
 @require_http_methods(["POST"])
@@ -39,3 +41,16 @@ def create_new_user(request: HttpRequest) -> HttpResponse:
         return HttpResponse(status=409, content="User already exists")
 
     return HttpResponse(status=201, content="User created correctly")
+
+
+@require_http_methods(["GET"])
+def get_all_users(request: HttpRequest) -> HttpResponse:
+    all_users = GetAllUsersUseCase().execute()
+
+    users_response = []
+    for user in all_users:
+        users_response.append(UserResponse.from_user(user).to_dict())
+
+    return HttpResponse(
+        status=200, content=json.dumps(users_response), content_type="application/json"
+    )
--- organizator_api/tests/users/infrastructure/persistance/test_orm_user_repository.py
+++ organizator_api/tests/users/infrastructure/persistance/test_orm_user_repository.py
@@ -1,4 +1,5 @@
 import uuid
+from datetime import datetime
 
 from tests.api_tests import ApiTests
 from tests.users.domain.UserFactory import UserFactory
@@ -32,3 +33,46 @@ class TestORMUserRepository(ApiTests):
         # Then
         with self.assertRaises(UserAlreadyExists):
             ORMUserRepository().create(user=user2)
+
+    def test__when_get_all__then_all_users_are_returned(self) -> None:
+        # Given
+        user = UserFactory().create()
+        user2 = UserFactory().create(
+            new_id=uuid.UUID("be0f4c18-4a7c-4c1e-8a62-fc50916b6c88"),
+            email="carlota@gmail.com",
+            username="carlota2",
+        )
+        ORMUserRepository().create(user=user)
+        ORMUserRepository().create(user=user2)
+
+        # When
+        users = ORMUserRepository().get_all()
+
+        # Then
+        self.assertEqual(len(users), 2)
+        self.assertEqual(users[0].id, user.id)
+        self.assertEqual(type(users[0].id), uuid.UUID)
+        self.assertEqual(users[0].email, user.email)
+        self.assertEqual(type(users[0].email), str)
+        self.assertEqual(users[0].password, user.password)
+        self.assertEqual(type(users[0].password), str)
+        self.assertEqual(users[0].first_name, user.first_name)
+        self.assertEqual(type(users[0].first_name), str)
+        self.assertEqual(users[0].last_name, user.last_name)
+        self.assertEqual(type(users[0].last_name), str)
+        self.assertEqual(users[0].username, user.username)
+        self.assertEqual(type(users[0].username), str)
+        self.assertEqual(users[0].bio, user.bio)
+        self.assertEqual(type(users[0].bio), str)
+        self.assertEqual(users[0].profile_image, user.profile_image)
+        self.assertEqual(type(users[0].profile_image), str)
+        self.assertEqual(type(users[0].created_at), datetime)
+        self.assertEqual(type(users[0].updated_at), datetime)
+        self.assertEqual(users[1].id, user2.id)
+        self.assertEqual(users[1].email, user2.email)
+        self.assertEqual(users[1].password, user2.password)
+        self.assertEqual(users[1].first_name, user2.first_name)
+        self.assertEqual(users[1].last_name, user2.last_name)
+        self.assertEqual(users[1].username, user2.username)
+        self.assertEqual(users[1].bio, user2.bio)
+        self.assertEqual(users[1].profile_image, user2.profile_image)
