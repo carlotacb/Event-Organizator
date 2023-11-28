aa0508ebcf355e9db1ccd7ece1f70ca627bf5a8d
Carlota Catot Bragós <carkbra@gmail.com>
2023-11-28 14:52:59 +0100

    minimal change (#25)

--- organizator/app/(tabs)/index.tsx
+++ organizator/app/(tabs)/index.tsx
@@ -7,6 +7,7 @@ export default function TabOneScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>And this is a subtitle to do a minimal test</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
