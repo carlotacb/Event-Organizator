0819531409c1c3e1bf84c9d757633d399964096a
Carlota Catot Bragós <carkbra@gmail.com>
2023-11-28 01:34:20 +0100

    view and tests created (#15)

--- organizator_api/app/users/infrastructure/http/urls.py
+++ organizator_api/app/users/infrastructure/http/urls.py
@@ -5,6 +5,7 @@ from app.users.infrastructure.http.views import (
     get_all_users,
     get_user_by_id,
     get_user_by_username,
+    update_user,
 )
 
 urlpatterns = [
@@ -12,4 +13,5 @@ urlpatterns = [
     path("", get_all_users),
     path("<uuid:user_id>", get_user_by_id),
     path("<str:username>", get_user_by_username),
+    path("update/<uuid:user_id>", update_user),
 ]
