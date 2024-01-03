848858415c3c4987eada069603a3aec582bca951
Carlota Catot Bragós <carkbra@gmail.com>
2024-01-03 15:27:45 +0100

    create view

--- organizator_api/app/users/infrastructure/http/urls.py
+++ organizator_api/app/users/infrastructure/http/urls.py
@@ -8,13 +8,14 @@ from app.users.infrastructure.http.views import (
     update_user,
     login,
     get_user_by_token,
-    logout,
+    logout, get_role_by_token,
 )
 
 urlpatterns = [
     path("new", create_new_user),
     path("", get_all_users),
     path("me", get_user_by_token),
+    path("me/role", get_role_by_token),
     path("login", login),
     path("logout", logout),
     path("update/<uuid:user_id>", update_user),
