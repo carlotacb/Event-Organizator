34d6d244b8bd8d8d847f35f5fb4b74c315a13930
Carlota Catot Bragós <carkbra@gmail.com>
2024-01-06 03:21:19 +0100

    update_user_use_case is now taking in account all the new fields

--- organizator_api/app/users/application/requests.py
+++ organizator_api/app/users/application/requests.py
@@ -21,8 +21,22 @@ class CreateUserRequest:
 
 @dataclass
 class UpdateUserRequest:
-    username: Optional[str] = None
     first_name: Optional[str] = None
     last_name: Optional[str] = None
+    username: Optional[str] = None
     bio: Optional[str] = None
     profile_image: Optional[str] = None
+    date_of_birth: Optional[str] = None
+    study: Optional[bool] = None
+    work: Optional[bool] = None
+    university: Optional[str] = None
+    degree: Optional[str] = None
+    expected_graduation: Optional[str] = None
+    current_job_role: Optional[str] = None
+    tshirt: Optional[str] = None
+    gender: Optional[str] = None
+    alimentary_restrictions: Optional[str] = None
+    github: Optional[str] = None
+    linkedin: Optional[str] = None
+    devpost: Optional[str] = None
+    webpage: Optional[str] = None
