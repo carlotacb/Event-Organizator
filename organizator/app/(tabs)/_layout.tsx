53a944704cc62b285ab68fcbf1bf688e67662ea0
Carlota Catot Bragós <carkbra@gmail.com>
2024-01-07 19:14:26 +0100

    Login and homepage redirects are now correct (#78)

--- organizator/app/(tabs)/_layout.tsx
+++ organizator/app/(tabs)/_layout.tsx
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Tabs } from "expo-router";
-import { FontAwesome5 } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
+import { getToken } from "../../utils/sessionCalls";
+import { getUserRole } from "../../utils/api/axiosUsers";
+import { UserRoles } from "../../utils/interfaces/Users";
 
 export default function HomepageTabs() {
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [isNotLogged, setIsNotLogged] = useState(false);
+
+  useEffect(() => {
+    const fetchAdminFunction = async () => {
+      const t = await getToken();
+      return getUserRole(t);
+    };
+
+    fetchAdminFunction().then((response) => {
+      if (response.error) {
+        setIsNotLogged(true);
+        return;
+      }
+      setIsAdmin(response.role === UserRoles.ORGANIZER_ADMIN);
+    });
+  }, []);
+
   return (
     <Tabs>
       <Tabs.Screen
@@ -12,7 +33,7 @@ export default function HomepageTabs() {
           headerTitle: "All upcoming events",
           headerShown: false,
           tabBarIcon: ({ color, size }) => (
-            <FontAwesome5 name="home" color={color} size={size} />
+            <FontAwesome name="home" color={color} size={size} />
           ),
         }}
       />
@@ -22,18 +43,22 @@ export default function HomepageTabs() {
           tabBarLabel: "News",
           headerShown: false,
           tabBarIcon: ({ color, size }) => (
-            <FontAwesome5 name="newspaper" color={color} size={size} />
+            <FontAwesome name="list" color={color} size={size} />
           ),
         }}
       />
       <Tabs.Screen
         name="profile"
         options={{
-          tabBarLabel: "Profile",
+          tabBarLabel: isNotLogged ? "Login" : "Profile",
           headerTitle: "My profile",
           headerShown: false,
           tabBarIcon: ({ color, size }) => (
-            <FontAwesome5 name="user" color={color} size={size} />
+            <FontAwesome
+              name={isNotLogged ? "sign-in" : "user"}
+              color={color}
+              size={size}
+            />
           ),
         }}
       />
