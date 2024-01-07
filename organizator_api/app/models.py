41121ca39244697b0a9f25cd8a0f0b992e2280df
Carlota Catot Bragós <carkbra@gmail.com>
2024-01-07 20:34:38 +0100

    [ORG-71] Application model and ORM model (#79)
    * create some structure and models for applications
    
    * apply black and migrations

--- organizator_api/app/models.py
+++ organizator_api/app/models.py
@@ -1,2 +1,5 @@
 from app.events.infrastructure.persistence.models.orm_event import ORMEvent
 from app.users.infrastructure.persistence.models.orm_user import ORMUser
+from app.applications.infrastructure.persistence.models.orm_application import (
+    ORMApplication,
+)
