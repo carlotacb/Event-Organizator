cd2d76e7f3996d792385d8d4c3ba07c66609f727
Carlota Catot Bragós <carkbra@gmail.com>
2024-01-08 12:39:44 +0100

    modify orm to add unique tuple of event, user

--- organizator_api/app/applications/infrastructure/persistence/models/orm_application.py
+++ organizator_api/app/applications/infrastructure/persistence/models/orm_application.py
@@ -4,6 +4,7 @@ from django.db import models
 class ORMEventApplication(models.Model):
     class Meta:
         db_table = "event_application"
+        unique_together = ["user", "event"]
 
     id = models.UUIDField(primary_key=True)
     user = models.ForeignKey("ORMUser", on_delete=models.CASCADE)
