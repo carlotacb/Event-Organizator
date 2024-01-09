bd7f59095706721ed0c741294133e09b7774d228
Carlota Catot Bragós <carkbra@gmail.com>
2024-01-09 16:47:50 +0100

    page in tab

--- organizator/app/(tabs)/manage/index.tsx
+++ organizator/app/(tabs)/manage/index.tsx
@@ -1,15 +1,59 @@
-import React from "react";
-import { View } from "react-native";
-import { Link } from "expo-router";
+import React, { useEffect, useState } from "react";
+import { Text, SafeAreaView, ScrollView } from "react-native";
+// @ts-ignore
+import styled from "styled-components/native";
+import { getToken } from "../../../utils/sessionCalls";
+import { getUserRole } from "../../../utils/api/axiosUsers";
+import { UserRoles } from "../../../utils/interfaces/Users";
+import EmptyPage from "../../../components/EmptyPage";
+import MyEventsPage from "../../../components/MyEventsPage";
 
-const ListPage = () => {
+const Container = styled(SafeAreaView)`
+  background-color: white;
+  flex: 1;
+`;
+export default function ListPage() {
+  const [isOrganizer, setIsOrganizer] = useState(false);
+  const [isNotLogged, setIsNotLogged] = useState(false);
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    const fetchAdminFunction = async () => {
+      const t = await getToken();
+      setToken(t || "");
+      return getUserRole(t);
+    };
+
+    fetchAdminFunction().then((response) => {
+      if (response.error) {
+        setIsNotLogged(true);
+        return;
+      }
+
+      setIsOrganizer(
+        response.role === UserRoles.ORGANIZER_ADMIN ||
+          response.role === UserRoles.ORGANIZER,
+      );
+    });
+  }, []);
+
   return (
-    <View>
-      <Link href="/list/1">News 1</Link>
-      <Link href="/list/2">News 2</Link>
-      <Link href="/list/3">News 3</Link>
-    </View>
+    <Container>
+      {isNotLogged ? (
+        <EmptyPage
+          title="You are not logged in"
+          subtitle="To see the events you are registered in you should log in first."
+          image={require("../../../assets/not-logged-in.png")}
+        />
+      ) : (
+        <ScrollView>
+          {isOrganizer ? (
+            <Text>This is the screen for the organizers</Text>
+          ) : (
+            <MyEventsPage />
+          )}
+        </ScrollView>
+      )}
+    </Container>
   );
-};
-
-export default ListPage;
+}
