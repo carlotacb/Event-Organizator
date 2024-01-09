e56d366a8ff868548c443c26c7174997146b53e3
Carlota Catot Bragós <carkbra@gmail.com>
2024-01-09 16:46:51 +0100

    Axios call for my events

--- organizator/utils/interfaces/Applications.ts
+++ organizator/utils/interfaces/Applications.ts
@@ -1,3 +1,17 @@
+import { EventAllInformation, EventInformation } from "./Events";
+
 export interface CreateNewApplicationResponse {
   readonly error: string | null;
+}
+
+export interface GetMyApplicationsResponse {
+  readonly error: string | null;
+  readonly applications: ApplicationInformationWithoutUser[] | null;
+}
+
+export interface ApplicationInformationWithoutUser {
+  readonly id: string;
+  readonly event: EventAllInformation;
+  readonly created_at: string;
+  readonly updated_at: string;
 }
