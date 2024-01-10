9e89544c8c7e2e562966b96a1cdc5094cff3ac22
Carlota Catot Bragós <carkbra@gmail.com>
2024-01-10 13:20:44 +0100

    add new values to the update page

--- organizator/utils/util-functions.ts
+++ organizator/utils/util-functions.ts
@@ -1,3 +1,15 @@
+export function formatDate(inputDate: string): string {
+  const date = new Date(inputDate);
+
+  const day = String(date.getDate()).padStart(2, "0");
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const year = String(date.getFullYear());
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+
+  return `${day}/${month}/${year} ${hours}:${minutes}`;
+}
+
 export function parseDate(dateAsString: string) {
   const date = new Date(dateAsString);
   return `${date.getDate().toString().padStart(2, "0")}-${date
