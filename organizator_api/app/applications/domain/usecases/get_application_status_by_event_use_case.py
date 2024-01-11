7ca67df1bc9ce98b0f1c946ba32257e59f3c02ea
Carlota Catot Bragós <carkbra@gmail.com>
2024-01-11 23:54:14 +0100

    Use case with new error codes

--- organizator_api/app/applications/domain/usecases/get_application_status_by_event_use_case.py
+++ organizator_api/app/applications/domain/usecases/get_application_status_by_event_use_case.py
@@ -1,10 +1,12 @@
 import uuid
 
+from app.applications.domain.exceptions import ApplicationNotFound, NotApplied, UserIsNotAParticipant
 from app.applications.domain.models.application import ApplicationStatus
 from app.applications.infrastructure.repository_factories import (
     ApplicationRepositoryFactory,
 )
 from app.events.domain.usecases.get_event_use_case import GetEventUseCase
+from app.users.domain.models.user import UserRoles
 from app.users.domain.usecases.get_user_by_token_use_case import GetUserByTokenUseCase
 
 
@@ -16,8 +18,14 @@ class GetApplicationStatusByEventUseCase:
         GetEventUseCase().execute(event_id=event_id)
         user = GetUserByTokenUseCase().execute(token=token)
 
-        application = self.application_repository.get_application(
-            event_id=event_id, user_id=user.id
-        )
+        try:
+            application = self.application_repository.get_application(
+                event_id=event_id, user_id=user.id
+            )
+        except ApplicationNotFound:
+            if user.role == UserRoles.PARTICIPANT:
+                raise NotApplied
+            else:
+                raise UserIsNotAParticipant
 
         return application.status
