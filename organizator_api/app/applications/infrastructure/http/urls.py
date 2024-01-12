e13e135296a4dc61f7f6cac761edaae56897a37a
Carlota Catot Bragós <carkbra@gmail.com>
2024-01-12 18:46:46 +0100

    View and url

--- organizator_api/app/applications/infrastructure/http/urls.py
+++ organizator_api/app/applications/infrastructure/http/urls.py
@@ -6,12 +6,14 @@ from app.applications.infrastructure.http.views import (
     get_applications_by_event,
     get_application_status,
     update_application_status,
+    cancel_application,
 )
 
 urlpatterns = [
     path("new", create_new_application),
     path("myevents", get_applications_by_token),
     path("update/<uuid:application_id>", update_application_status),
+    path("cancel/<uuid:application_id>", cancel_application),
     path("status/<uuid:event_id>", get_application_status),
     path("participants/<uuid:event_id>", get_applications_by_event),
 ]
