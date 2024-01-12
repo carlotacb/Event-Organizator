fcf26765c8c0e8b7f91137d81bfbf66cd20ca33c
Carlota Catot Bragós <carkbra@gmail.com>
2024-01-13 00:36:56 +0100

    myevents card now have onpress confirm

--- organizator/components/CardMyEvents.tsx
+++ organizator/components/CardMyEvents.tsx
@@ -2,8 +2,7 @@ import React from "react";
 // @ts-ignore
 import styled from "styled-components/native";
 import { FontAwesome5 } from "@expo/vector-icons";
-import { Pressable, Text } from "react-native";
-import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Pressable } from "react-native";
 import {
   getColorForApplicationStatus,
   parseDate,
@@ -17,6 +16,8 @@ interface CardProps {
   id: string;
   setShowCancelAlert: (showCancelAlert: boolean) => void;
   setIdToCancel: (idToCancel: string) => void;
+  setShowConfirmAlert: (showConfirmAlert: boolean) => void;
+  setIdToConfirm: (idToConfirm: string) => void;
 }
 
 const CardContainer = styled.View<{ isPast: boolean }>`
@@ -26,12 +27,12 @@ const CardContainer = styled.View<{ isPast: boolean }>`
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.25);
   border-radius: 20px;
   margin: 10px;
-  width: 280px;
+  width: 300px;
   opacity: ${(props: { isPast: boolean }) => (props.isPast ? 0.5 : 1)};
 `;
 
 const CardImage = styled.Image`
-  height: 150px;
+  height: 200px;
   border-radius: 20px;
 `;
 
@@ -58,27 +59,13 @@ const TextLine = styled.View`
   margin-top: 10px;
 `;
 
-const PastTag = styled.View`
-  position: absolute;
-  top: 25px;
-  right: 30px;
-  border: 4px solid #772323;
-  background-color: rgba(119, 35, 35, 0.8);
-  border-radius: 20px;
-  padding: 5px 15px;
-`;
-
-const PastText = styled.Text`
-  color: white;
-  font-weight: bold;
-  font-size: 20px;
-`;
-
 const TagContainer = styled.View`
   display: flex;
   width: 100%;
   margin-top: 20px;
   align-items: flex-end;
+  position: absolute;
+  right: 15px;
 `;
 
 const TagStatus = styled.View<{ backgroundColor: string }>`
@@ -88,12 +75,17 @@ const TagStatus = styled.View<{ backgroundColor: string }>`
     props.backgroundColor};
   padding: 5px 10px;
   border-radius: 20px;
-  color: white;
   text-align: center;
+`;
+
+const TagText = styled.Text`
+  font-weight: bold;
+  font-size: 16px;
 `;
 
 const ButtonsContainer = styled.View`
   display: flex;
+  flex-direction: column;
   width: 100%;
   justify-content: center;
   align-items: center;
@@ -104,12 +96,26 @@ const CancelButton = styled(Pressable)`
   flex-direction: row;
   align-items: center;
   justify-content: center;
-  padding: 10px 30px;
-  margin-top: 20px;
+  padding: 10px;
+  margin-top: 15px;
   border-radius: 20px;
   gap: 10px;
+  width: 100%;
   background-color: #a65858;
-  width: 50%;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.25);
+`;
+
+const ConfirmButton = styled(Pressable)`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  padding: 10px;
+  margin-top: 15px;
+  border-radius: 20px;
+  gap: 10px;
+  width: 100%;
+  background-color: #60a658;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.25);
 `;
 
@@ -128,47 +134,52 @@ export default function CardMyEvents(props: CardProps) {
     id,
     setIdToCancel,
     setShowCancelAlert,
+    setIdToConfirm,
+    setShowConfirmAlert,
   } = props;
 
   const isPast = () => startDate < new Date().toISOString();
 
   return (
-    <>
-      <CardContainer isPast={isPast()}>
-        <CardImage source={{ uri: headerImage }} />
-        <CardTextContainer>
-          <CardTitle>{title}</CardTitle>
-          <TextLine>
-            <FontAwesome5 name="calendar" size={16} />
-            <CardText>{parseDate(startDate)}</CardText>
-          </TextLine>
-          <TagContainer>
-            <TagStatus backgroundColor={getColorForApplicationStatus(status)}>
-              <Text>{status}</Text>
-            </TagStatus>
-          </TagContainer>
-          <ButtonsContainer>
-            {status !== "Cancelled" &&
-              status !== "Rejected" &&
-              status !== "Invalid" && (
-                <CancelButton
-                  onPress={() => {
-                    setIdToCancel(id);
-                    setShowCancelAlert(true);
-                  }}
-                >
-                  <FontAwesome name="close" size={16} color="white" />
-                  <CancelButtonText>Cancel</CancelButtonText>
-                </CancelButton>
-              )}
-          </ButtonsContainer>
-        </CardTextContainer>
-      </CardContainer>
-      {isPast() && (
-        <PastTag>
-          <PastText>Past</PastText>
-        </PastTag>
-      )}
-    </>
+    <CardContainer isPast={isPast()}>
+      <CardImage source={{ uri: headerImage }} />
+      <CardTextContainer>
+        <CardTitle>{title}</CardTitle>
+        <TextLine>
+          <FontAwesome5 name="calendar" size={16} />
+          <CardText>{parseDate(startDate)}</CardText>
+        </TextLine>
+        <ButtonsContainer>
+          {status === "Invited" && !isPast() && (
+            <ConfirmButton
+              onPress={() => {
+                setIdToConfirm(id);
+                setShowConfirmAlert(true);
+              }}
+            >
+              <CancelButtonText>Confirm participation</CancelButtonText>
+            </ConfirmButton>
+          )}
+          {status !== "Cancelled" &&
+            status !== "Rejected" &&
+            status !== "Invalid" &&
+            !isPast() && (
+              <CancelButton
+                onPress={() => {
+                  setIdToCancel(id);
+                  setShowCancelAlert(true);
+                }}
+              >
+                <CancelButtonText>Cancel participation</CancelButtonText>
+              </CancelButton>
+            )}
+        </ButtonsContainer>
+      </CardTextContainer>
+      <TagContainer>
+        <TagStatus backgroundColor={getColorForApplicationStatus(status)}>
+          <TagText>{status}</TagText>
+        </TagStatus>
+      </TagContainer>
+    </CardContainer>
   );
 }
