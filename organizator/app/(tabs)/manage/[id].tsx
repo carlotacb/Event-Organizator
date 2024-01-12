81801367338d6142a88a2e24a6c9d8c6288170c9
Carlota Catot Bragós <carkbra@gmail.com>
2024-01-12 13:32:43 +0100

    filters

--- organizator/app/(tabs)/manage/[id].tsx
+++ organizator/app/(tabs)/manage/[id].tsx
@@ -10,6 +10,7 @@ import { getToken } from "../../../utils/sessionCalls";
 import { getParticipants } from "../../../utils/api/axiosApplications";
 import { ParticipantsInformation } from "../../../utils/interfaces/Applications";
 import EmptyPage from "../../../components/EmptyPage";
+import FilterButton from "../../../components/FilterButtons";
 
 const Container = styled(SafeAreaView)`
   background-color: white;
@@ -47,12 +48,53 @@ const TagStatus = styled(View)<{ backgroundColor: string }>`
   color: white;
 `;
 
+const ButtonsContainer = styled(View)`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 5px;
+  margin-bottom: 20px;
+`;
+
+const ParticipantsNumber = styled(Text)`
+  text-align: right;
+  font-size: 16px;
+  margin-bottom: 20px;
+  margin-right: 20px;
+  font-style: italic;
+  color: dimgray;
+`;
+
 export default function Id() {
   const { id } = useLocalSearchParams();
   const [loading, setLoading] = useState(true);
   const [applications, setApplications] = useState<
     ParticipantsInformation[] | null
   >(null);
+  const [allApplications, setAllApplications] = useState<
+    ParticipantsInformation[] | null
+  >(null);
+  const [stats, setStats] = useState({
+    all: 0,
+    confirmed: 0,
+    rejected: 0,
+    underReview: 0,
+    invited: 0,
+    cancelled: 0,
+    invalid: 0,
+    waitList: 0,
+  });
+  const [active, setActive] = useState({
+    all: true,
+    confirmed: false,
+    rejected: false,
+    underReview: false,
+    invited: false,
+    cancelled: false,
+    invalid: false,
+    waitList: false,
+  });
 
   useEffect(() => {
     const fetchData = async () => {
@@ -62,9 +104,41 @@ export default function Id() {
     };
 
     fetchData().then((response) => {
-      console.log(response);
       setLoading(false);
+      setAllApplications(response.participants || []);
       setApplications(response.participants || []);
+
+      setStats({
+        all: response.participants?.length || 0,
+        confirmed:
+          response.participants?.filter(
+            (participant) => participant.status === "Confirmed",
+          ).length || 0,
+        rejected:
+          response.participants?.filter(
+            (participant) => participant.status === "Rejected",
+          ).length || 0,
+        underReview:
+          response.participants?.filter(
+            (participant) => participant.status === "Under review",
+          ).length || 0,
+        invited:
+          response.participants?.filter(
+            (participant) => participant.status === "Invited",
+          ).length || 0,
+        cancelled:
+          response.participants?.filter(
+            (participant) => participant.status === "Cancelled",
+          ).length || 0,
+        invalid:
+          response.participants?.filter(
+            (participant) => participant.status === "Invalid",
+          ).length || 0,
+        waitList:
+          response.participants?.filter(
+            (participant) => participant.status === "Wait list",
+          ).length || 0,
+      });
     });
   }, []);
 
@@ -87,6 +161,190 @@ export default function Id() {
           <LoadingPage />
         ) : (
           <View>
+            <ButtonsContainer>
+              <FilterButton
+                title={`All (${stats.all})`}
+                onPress={() => {
+                  setApplications(allApplications);
+                  setActive(() => ({
+                    all: true,
+                    confirmed: false,
+                    rejected: false,
+                    underReview: false,
+                    invited: false,
+                    cancelled: false,
+                    invalid: false,
+                    waitList: false,
+                  }));
+                }}
+                color="#040240"
+                active={active.all}
+              />
+              <FilterButton
+                title={`Under Review (${stats.rejected})`}
+                onPress={() => {
+                  setApplications(
+                    applications?.filter(
+                      (participant) => participant.status === "Under Review",
+                    ) || [],
+                  );
+
+                  setActive(() => ({
+                    all: false,
+                    confirmed: false,
+                    rejected: false,
+                    underReview: true,
+                    invited: false,
+                    cancelled: false,
+                    invalid: false,
+                    waitList: false,
+                  }));
+                }}
+                color={getTagColor("Under review")}
+                active={active.underReview}
+              />
+              <FilterButton
+                title={`Invited (${stats.invited})`}
+                onPress={() => {
+                  setApplications(
+                    applications?.filter(
+                      (participant) => participant.status === "Invited",
+                    ) || [],
+                  );
+                  setActive(() => ({
+                    all: false,
+                    confirmed: false,
+                    rejected: false,
+                    underReview: false,
+                    invited: true,
+                    cancelled: false,
+                    invalid: false,
+                    waitList: false,
+                  }));
+                }}
+                color={getTagColor("Invited")}
+                active={active.invited}
+              />
+              <FilterButton
+                title={`Confirmed (${stats.confirmed})`}
+                onPress={() => {
+                  setApplications(
+                    applications?.filter(
+                      (participant) => participant.status === "Confirmed",
+                    ) || [],
+                  );
+
+                  setActive(() => ({
+                    all: false,
+                    confirmed: true,
+                    rejected: false,
+                    underReview: false,
+                    invited: false,
+                    cancelled: false,
+                    invalid: false,
+                    waitList: false,
+                  }));
+                }}
+                color={getTagColor("Confirmed")}
+                active={active.confirmed}
+              />
+              <FilterButton
+                title={`Cancelled (${stats.cancelled})`}
+                onPress={() => {
+                  setApplications(
+                    applications?.filter(
+                      (participant) => participant.status === "Cancelled",
+                    ) || [],
+                  );
+
+                  setActive(() => ({
+                    all: false,
+                    confirmed: false,
+                    rejected: false,
+                    underReview: false,
+                    invited: false,
+                    cancelled: true,
+                    invalid: false,
+                    waitList: false,
+                  }));
+                }}
+                color={getTagColor("Cancelled")}
+                active={active.cancelled}
+              />
+              <FilterButton
+                title={`Rejected (${stats.rejected})`}
+                onPress={() => {
+                  setApplications(
+                    applications?.filter(
+                      (participant) => participant.status === "Rejected",
+                    ) || [],
+                  );
+
+                  setActive(() => ({
+                    all: false,
+                    confirmed: false,
+                    rejected: true,
+                    underReview: false,
+                    invited: false,
+                    cancelled: false,
+                    invalid: false,
+                    waitList: false,
+                  }));
+                }}
+                color={getTagColor("Rejected")}
+                active={active.rejected}
+              />
+              <FilterButton
+                title={`Invalid (${stats.rejected})`}
+                onPress={() => {
+                  setApplications(
+                    applications?.filter(
+                      (participant) => participant.status === "Invalid",
+                    ) || [],
+                  );
+
+                  setActive(() => ({
+                    all: false,
+                    confirmed: false,
+                    rejected: false,
+                    underReview: false,
+                    invited: false,
+                    cancelled: false,
+                    invalid: true,
+                    waitList: false,
+                  }));
+                }}
+                color={getTagColor("Invalid")}
+                active={active.invalid}
+              />
+              <FilterButton
+                title={`Wait (${stats.waitList})`}
+                onPress={() => {
+                  setApplications(
+                    applications?.filter(
+                      (participant) => participant.status === "Wait list",
+                    ) || [],
+                  );
+
+                  setActive(() => ({
+                    all: false,
+                    confirmed: false,
+                    rejected: false,
+                    underReview: false,
+                    invited: false,
+                    cancelled: false,
+                    invalid: false,
+                    waitList: true,
+                  }));
+                }}
+                color={getTagColor("Wait list")}
+                active={active.waitList}
+              />
+            </ButtonsContainer>
+            <ParticipantsNumber>
+              Total Participants: {applications?.length}
+            </ParticipantsNumber>
+
             {applications?.length === 0 ? (
               <EmptyPage
                 title="No participants"
