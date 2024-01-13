17d77a3950bf8da5decce9059850132e50415b17
Carlota Catot Bragós <carkbra@gmail.com>
2024-01-13 13:23:11 +0100

    add tokens for mediaqueries

--- organizator/components/componentsStyled/tokens.ts
+++ organizator/components/componentsStyled/tokens.ts
@@ -4,3 +4,19 @@ export const systemColors = {
   accept: "#58a659",
   action: "#102275",
 };
+
+const breakpoints = {
+  xs: "320px",
+  sm: "640px",
+  md: "768px",
+  lg: "1024px",
+  xl: "1280px",
+};
+
+export const devices = {
+  mobile: `(min-width: ${breakpoints.xs})`,
+  tablet: `(min-width: ${breakpoints.sm})`,
+  smallComputer: `(min-width: ${breakpoints.md})`,
+  computer: `(min-width: ${breakpoints.lg})`,
+  large: `(min-width: ${breakpoints.xl})`,
+};
