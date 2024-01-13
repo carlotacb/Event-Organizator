4283d0c2fdaee9922ddd26b1f9359549d1811dd4
Carlota Catot Bragós <carkbra@gmail.com>
2024-01-13 18:38:17 +0100

    remove unused components

--- organizator/app/_layout.tsx
+++ organizator/app/_layout.tsx
@@ -17,7 +17,6 @@ export default function RootLayout() {
         }}
       />
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="(tabs2)" options={{ headerShown: false }} />
     </Stack>
   );
 }
