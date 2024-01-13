013228c2314e846cdb1c43f930b6818f938ba622
Carlota Catot Bragós <carkbra@gmail.com>
2024-01-13 22:48:34 +0100

    going to prod

--- organizator/utils/api/constants.ts
+++ organizator/utils/api/constants.ts
@@ -1,8 +1,9 @@
 // Local development
-const usingURL = "http://0.0.0.0:8000/organizator-api";
+// const usingURL = "http://0.0.0.0:8000/organizator-api";
 
 // Production
-// const usingURL = "https://event-organizator-api-c6wz5mj4uq-ew.a.run.app/organizator-api";
+const usingURL =
+  "https://event-organizator-api-c6wz5mj4uq-ew.a.run.app/organizator-api";
 
 export const usersAPI = `${usingURL}/users`;
 export const eventsAPI = `${usingURL}/events`;
