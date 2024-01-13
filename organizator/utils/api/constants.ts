6f62bd1022e7a080a0f8a64209f8ea6cc87a45df
Carlota Catot Bragós <carkbra@gmail.com>
2024-01-13 19:06:09 +0100

    change constants to development in local

--- organizator/utils/api/constants.ts
+++ organizator/utils/api/constants.ts
@@ -1,8 +1,8 @@
-const baseURLdevelopment = "http://0.0.0.0:8000/organizator-api";
-const baseURLproduction =
-  "https://event-organizator-api-c6wz5mj4uq-ew.a.run.app/organizator-api";
+// Local development
+const usingURL = "http://0.0.0.0:8000/organizator-api";
 
-const usingURL = baseURLproduction;
+// Production
+// const usingURL = "https://event-organizator-api-c6wz5mj4uq-ew.a.run.app/organizator-api";
 
 export const usersAPI = `${usingURL}/users`;
 export const eventsAPI = `${usingURL}/events`;
