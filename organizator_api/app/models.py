be4cdf1ef7e48ffd6196f8ed89161d9ee3ead70f
Carlota Catot Bragós <carkbra@gmail.com>
2024-01-17 17:18:36 +0100

    model in general models

--- organizator_api/app/models.py
+++ organizator_api/app/models.py
@@ -3,3 +3,4 @@ from app.users.infrastructure.persistence.models.orm_user import ORMUser
 from app.applications.infrastructure.persistence.models.orm_application import (
     ORMEventApplication,
 )
+from app.questions.infrastructure.persistence.model.orm_question import ORMQuestion
