68b3059b4eeb6fc5a65a4ca4be2460a5fe215803
Carlota Catot Bragós <carkbra@gmail.com>
2024-01-17 20:04:17 +0100

    views

--- organizator_api/app/questions/infrastructure/http/urls.py
+++ organizator_api/app/questions/infrastructure/http/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from app.questions.infrastructure.http.views import create_new_question
+from app.questions.infrastructure.http.views import create_new_question, update_question
 
 urlpatterns = [
     path("new", create_new_question),
+    path("update/<str:question_id>", update_question)
 ]
