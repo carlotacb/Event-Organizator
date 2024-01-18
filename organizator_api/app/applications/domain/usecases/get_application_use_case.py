8332456ad8d6b4f12148e49529969256096c57fd
Carlota Catot Bragós <carkbra@gmail.com>
2024-01-18 01:24:38 +0100

    get application by id

---
+++ organizator_api/app/applications/domain/usecases/get_application_use_case.py
@@ -0,0 +1,12 @@
+import uuid
+
+from app.applications.domain.models.application import Application
+from app.applications.infrastructure.repository_factories import ApplicationRepositoryFactory
+
+
+class GetApplicationUseCase:
+    def __init__(self) -> None:
+        self.application_repository = ApplicationRepositoryFactory.create()
+
+    def execute(self, application_id: uuid.UUID) -> Application:
+        return self.application_repository.get(application_id)
